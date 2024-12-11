e50f953bbd0cf6b79493002c1208391af482f160
kanika <kanikac363@gmail.com>
2024-12-11 22:40:07 +0530

    create the header section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,11 +6,13 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header></Header>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
